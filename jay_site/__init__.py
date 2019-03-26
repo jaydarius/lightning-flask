721f5fe4a8d08d813fe838587f4a9a736d490191
Jaydarian <jayrey.go@gmail.com>
2019-03-26 00:00:21 -0500

    create blueprints

--- jay_site/__init__.py
+++ jay_site/__init__.py
@@ -1,7 +1,10 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)  # we create our Flask instance once on the package init
 app.config['SECRET_KEY'] = '9ada615921b3d25d6cca4208129edce0'
@@ -11,5 +14,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
 
 from jay_site import routes
