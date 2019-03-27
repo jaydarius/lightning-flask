cefbc6c5162e26b318991f34c7a4c8868dd54ced
Jaydarian <jayrey.go@gmail.com>
2019-03-26 22:22:10 -0500

    create config.py

--- jay_site/__init__.py
+++ jay_site/__init__.py
@@ -6,20 +6,22 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
+from jay_site.config import Config
+
 app = Flask(__name__)  # we create our Flask instance once on the package init
-app.config['SECRET_KEY'] = '9ada615921b3d25d6cca4208129edce0'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # the 3 slashes are a relative path from the current file
+app.config.from_object(Config)
 db = SQLAlchemy(app)  # we create our SQL Map instance once on the package init
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
 mail = Mail(app)
 
+from jay_site.users.routes import users
+from jay_site.posts.routes import posts
+from jay_site.main.routes import main
 
-from jay_site import routes
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
--- jay_site/users/routes.py
+++ jay_site/users/routes.py
@@ -4,7 +4,7 @@ from flask_mail import Message
 
 from jay_site import app, db, bcrypt, mail  # read: from our program initialization import our jay_site objects
 from jay_site.users.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
-                            PostForm, RequestResetForm, ResetPasswordForm)
+                                  RequestResetForm, ResetPasswordForm)
 from jay_site.models import User, Post
 from jay_site.users.utils import save_picture, send_reset_email
 
@@ -14,7 +14,7 @@ users = Blueprint('users', __name__)
 @users.route("/register", methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('main.home'))
     form = RegistrationForm()
     if form.validate_on_submit(): 
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -22,21 +22,21 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash(f'Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
+        return redirect(url_for('users.login'))
     return render_template('register.html', title='Register', form=form)  # if method == 'GET'
 
 
 @users.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('main.home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')  # `args` is a dict. Use get in case the `next` key does not exist
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -44,7 +44,7 @@ def login():
 @users.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('main.home'))
 
 @users.route("/account", methods=['GET', 'POST'])
 @login_required
@@ -58,7 +58,7 @@ def account():
         current_user.email = form.email.data
         db.session.commit()  # current_user is already part of db.session
         flash('Your account has been updated!', 'success')
-        return redirect(url_for('account'))
+        return redirect(url_for('users.account'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
@@ -78,28 +78,28 @@ def user_posts(username):
 @users.route("/reset_password", methods=['GET', 'POST'])
 def reset_request():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('main.home'))
     form = RequestResetForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         send_reset_email(user)
         flash('An email has been sent with instructions to reset your password', 'info')
-        return redirect(url_for('login'))
+        return redirect(url_for('users.login'))
     return render_template('reset_request.html', title='Reset Password', form=form)
 
 @users.route("/reset_password/<token>", methods=['GET', 'POST'])
 def reset_token(token):
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('main.home'))
     user = User.verify_reset_token(token)
     if user is None:
         flash('That is an invalid or expired token', 'warning')
-        return redirect(url_for('reset_request'))
+        return redirect(url_for('users.reset_request'))
     form = ResetPasswordForm()
     if form.validate_on_submit(): 
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user.password = hashed_password
         db.session.commit()
         flash(f'Your password has been updated! You are now able to log in', 'success')
-        return redirect(url_for('login'))
+        return redirect(url_for('users.login'))
     return render_template('reset_token.html', title='Reset Password', form=form)
