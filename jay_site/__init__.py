44ab22a5ab5c548e3b16d5439e303a5c9eac8f53
Jaydarian <jayrey.go@gmail.com>
2019-03-27 00:14:46 -0500

    create errors package

--- jay_site/__init__.py
+++ jay_site/__init__.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -8,20 +6,29 @@ from flask_mail import Mail
 
 from jay_site.config import Config
 
-app = Flask(__name__)  # we create our Flask instance once on the package init
-app.config.from_object(Config)
-db = SQLAlchemy(app)  # we create our SQL Map instance once on the package init
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()  # we create our SQL Map instance once on the package init
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-mail = Mail(app)
+mail = Mail()
 
-from jay_site.users.routes import users
-from jay_site.posts.routes import posts
-from jay_site.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from jay_site.users.routes import users
+    from jay_site.posts.routes import posts
+    from jay_site.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
